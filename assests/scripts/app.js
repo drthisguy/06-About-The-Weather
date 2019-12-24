6551370b8b5bcaefc46dbf909c4ea4006b78ec08
My Lord <paget@sapling-inc.local>
2019-12-24 12:38:45 -0500

    DOM classes and IDs added. changed weather API query methode.  seach btn add and functional.

--- assests/scripts/app.js
+++ assests/scripts/app.js
@@ -3,8 +3,8 @@ $(document).ready( function() {
     var geo = navigator.geolocation
         googApiKey = 'AIzaSyCI3zv9mMZuVUPGueGVIYUyD3etz0VJK7I',
         weatherKey = '63df9b45298d8782d0474639d201179f',
-        city= 'Philadelphia',
-        country= 'PA',
+        city= '',
+        country= '',
         lat= '',
         lng = '';
 
@@ -12,31 +12,50 @@ $(document).ready( function() {
         geo.getCurrentPosition(function(position) {
         lat = position.coords.latitude;
         lng = position.coords.longitude;
+
+        // url: "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid="+weatherKey+"",
     })};
-    getCoordinates();
-    getWeather();
-    function getCoordinates() {
-       
-       $.ajax({
+    // getWeather();
+    function getWeather() {
+
+
+       //get location specifics from google first. This will allow for more definite results. Users can misspell input, use state or country OR neither.  They can also use abbreviations.. (e.g. 'philly'), etc. 
+       $.ajax({       
                url: "https://maps.googleapis.com/maps/api/geocode/json?address="+city+",+"+country+"&key="+googApiKey+"",
                method: "GET"
              })
                .then(function(response) {
                  console.log(response);
-                 lat = response.results[0].geometry.location.lat; 
-                 lng = response.results[0].geometry.location.lng;
-           });
-    }
-    function getWeather() {
-        $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid="+weatherKey+"",
-                method: "GET"
-              })
-                .then(function(response) {
-                  console.log(response);
-                  
-            });
-    }
+                 var city = response.results[0].address_components[0].long_name;
+                 var country = response.results[0].address_components[2].long_name; 
+                 
+   
+            $.ajax({    //use coordinates to get weather from openweather.
+                    url: "http://api.openweathermap.org/data/2.5/forecast?q="+city+","+country+"&APPID="+weatherKey+"",
+                    method: "GET"
+                })
+                    .then(function(weather) {
+                        console.log(weather);
+                        
+                    console.log(city, country);
+                    paintWeather(weather)
+                 });
+                
+          })
+        
+    };
+
+        function paintWeather(weather) {
+          var temp = document.querySelector('.temp'),
+              location = document.querySelector('.location'),
+              description = document.querySelector('.description'),
+              high = document.querySelector('.high'),
+              low = document.querySelector('.low'),
+              icon = document.querySelector('.icon'),
+              wind = document.querySelector('.wind');
+
+         };
+
 
       function getPlaces() {
         var places;
@@ -47,4 +66,20 @@ $(document).ready( function() {
           places = JSON.parse(localStorage.getItem("places"));
         }
       }
-});
+
+      function changeLocation(newCity, newCountry) {
+        city = newCity;
+        country = newCountry;
+    }
+
+      $('#search-btn').click(function() {
+          var city = $(this).siblings('#city-field').val().trim(),
+              country = $(this).siblings('#country-field').val().trim();
+        changeLocation(city, country);
+          
+            var weather = getWeather();
+            console.log(weather);
+            
+      })
+
+    });
